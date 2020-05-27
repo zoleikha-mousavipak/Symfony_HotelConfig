40593facff16d92b54f80619c79e52291b8f018e
zoleikha <zmpak2000@gmail.com>
2020-05-27 17:07:25 +0200

    uuid

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Hotel;
 use App\Entity\Review;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
 
 class AppFixtures extends Fixture
 {
@@ -19,35 +20,35 @@ class AppFixtures extends Fixture
     public function loadHotels($manager)
     {
         $hotel = new Hotel();
-        $hotel->setId(1);
+        $hotel->setId(Uuid::uuid4());
         $hotel->setName('Hotel Alexanderplatz');
         $hotel->setAddress('Alexanderplatz 1, 10409, Berlin');
 
         $manager->persist($hotel);
 
         $hotel = new Hotel();
-        $hotel->setId(2);
+        $hotel->setId(Uuid::uuid4());
         $hotel->setName('Hotel Alexanderplatz');
         $hotel->setAddress('Alexanderplatz 1, 10409, Berlin');
 
         $manager->persist($hotel);
 
         $hotel = new Hotel();
-        $hotel->setId(3);
+        $hotel->setId(Uuid::uuid4());
         $hotel->setName('Hotel Alexanderplatz');
         $hotel->setAddress('Alexanderplatz 1, 10409, Berlin');
 
         $manager->persist($hotel);
 
         $hotel = new Hotel();
-        $hotel->setId(4);
+        $hotel->setId(Uuid::uuid4());
         $hotel->setName('Hotel Alexanderplatz');
         $hotel->setAddress('Alexanderplatz 1, 10409, Berlin');
 
         $manager->persist($hotel);
 
         $hotel = new Hotel();
-        $hotel->setId(5);
+        $hotel->setId(Uuid::uuid4());
         $hotel->setName('Hotel Alexanderplatz');
         $hotel->setAddress('Alexanderplatz 1, 10409, Berlin');
 
@@ -58,38 +59,38 @@ class AppFixtures extends Fixture
     {
         // hotel 1
         $review = new Review();
-        $review->setHotelId(1);
+        $review->setHotel($this->Hotel[0]);
         $review->setComment('Very nice stay');
         $review->setScore(10);
         $manager->persist($review);
 
         $review = new Review();
-        $review->setHotelId(1);
+        $review->setHotel($this->Hotel[0]);
         $review->setComment('Average');
         $review->setScore(5);
         $manager->persist($review);
 
         $review = new Review();
-        $review->setHotelId(1);
+        $review->setHotel($this->Hotel[0]);
         $review->setComment('Very nice stay, I enjoyed it a lot.');
         $review->setScore(9);
         $manager->persist($review);
 
         $review = new Review();
-        $review->setHotelId(1);
+        $review->setHotel($this->Hotel[0]);
         $review->setComment('Worst experience ever.');
         $review->setScore(1);
         $manager->persist($review);
 
         // hotel 2
         $review = new Review();
-        $review->setHotelId(2);
+        $review->setHotel($this->Hotel[1]);
         $review->setComment('The receptionist was not smiling.');
         $review->setScore(5);
         $manager->persist($review);
 
         $review = new Review();
-        $review->setHotelId(2);
+        $review->setHotel($this->Hotel[1]);
         $review->setComment('Very nice stay, the reception was really fast.');
         $review->setScore(10);
         $manager->persist($review);
